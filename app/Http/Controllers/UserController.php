63a7a841fa8f3d40f554bcc841ec968e6e6f72a7
Heickel Guerfal <heickel.guerfal@gmail.com>
2022-06-15 11:11:26 +0100

    feat: make create function in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,85 +29,11 @@ class UserController extends Controller
         return view('user.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $validatedData = $request->validate([
-            'firstname' => 'required',
-            'lastname' => 'required',
-            'email' => 'required|email|unique:users,email',
-            'phone' => 'required|min:8|max:11|regex:/^([0-9\s\-\+\(\)]*)$/',
-            'role' => 'required',
-            'password' => 'required|confirmed'
-        ]);
-        $show = User::create($validatedData);
 
-        return redirect('/users')->with('success', 'User is successfully saved');
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $user = User::findOrFail($id);
 
-        return view('user.edit', compact('user'));
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $validatedData = $request->validate([
-            'firstname' => 'required',
-            'lastname' => 'required',
-            'email' => 'required|email',
-            'phone' => 'required|min:8|max:11|regex:/^([0-9\s\-\+\(\)]*)$/',
-            'role' => 'required',
-        ]);
-        User::whereId($id)->update($validatedData);
-        return redirect('/users')->with('success', 'User Data is successfully updated');
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $user = User::findOrFail($id);
-        $user->delete();
 
-        return redirect('/users')->with('success', 'User Data is successfully deleted');
-    }
 }
