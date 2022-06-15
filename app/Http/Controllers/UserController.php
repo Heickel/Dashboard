cf1e875e64d491e62926462b2c6e35aaab21354a
Heickel Guerfal <heickel.guerfal@gmail.com>
2022-06-15 16:53:10 +0100

    refactor: destroy function is now sofdelete in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -105,9 +105,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        $user = User::findOrFail($id);
-        $user->delete();
+        User::find($id)->delete();
 
         return redirect('/users')->with('success', 'User Data is successfully deleted');
     }
+
 }
