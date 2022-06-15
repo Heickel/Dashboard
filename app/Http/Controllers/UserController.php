a620b3acc3d9c55429153c3b7f1a405bf5a18e62
Heickel Guerfal <heickel.guerfal@gmail.com>
2022-06-15 11:11:49 +0100

    feat: create store function in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,26 @@ class UserController extends Controller
         return view('user.create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|min:8|max:11|regex:/^([0-9\s\-\+\(\)]*)$/',
+            'role' => 'required',
+            'password' => 'required|confirmed'
+        ]);
+        $show = User::create($validatedData);
 
+        return redirect('/users')->with('success', 'User is successfully saved');
+    }
 
 
 
