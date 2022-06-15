a03dc454e866279b1e4bae8df4dcec9009bc160e
Heickel Guerfal <heickel.guerfal@gmail.com>
2022-06-15 17:20:34 +0100

    feat: create routes for user functions

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('users', 'UserController');
+Route::get('/users/delete/{id}', 'UserController@destroy')
+     ->name('users.destroy');
+
 Route::get('/', function () {
     return view('welcome');
 });
