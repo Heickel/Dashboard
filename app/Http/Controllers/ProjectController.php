857ee941850d946ab155829469b703d2d6e2f690
nihelgh <73757674+nihelgh@users.noreply.github.com>
2022-06-17 15:33:29 +0100

    refactor: add functions to update project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -36,5 +36,31 @@ class ProjectController extends Controller
             Log::critical("message".$e->getMessage());
         }
     }
+
+    public function edit(Project $project)
+    {
+        try{        
+        return view('add-project-details-form.edit',compact('project'));
+        }catch(Exception $e){
+        Log::critical("edit project error".$e->getMessage());
+        }
+    }
+
+
+    public function update(Request $request, Project $project)
+    {
+        try{
+            $request->validate([
+                'name' => 'required|max:100',
+                'code' => 'required',
+            ]);
+    
+        if($project->update($request->all())){
+            return redirect()->route('add-project-details-form.index')->with('status', 'Projects Details Form Data Has Been updated');
+        }     
+        }catch(Exception $e){
+                        Log::critical("message".$e->getMessage());
+        }
+    }
 }
 
