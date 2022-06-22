0771f21b05325c452a5a328bab44e4fce6d77621
Heickel Guerfal <heickel.guerfal@gmail.com>
2022-06-22 15:01:16 +0100

    feat: create seeder for supplier

---
+++ database/seeders/SupplierSeeder.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Supplier;
+
+class SupplierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Supplier::create([
+            'name' => ' مؤسسة الحياة والحيوان للتجارة',
+        ]);
+        Supplier::create([
+            'name' => 'مؤسسة دار دلتا',
+        ]);
+        Supplier::create([
+            'name' => 'مؤسسة رحاب التقوى للمقاولات',
+        ]);
+        Supplier::create([
+            'name' => 'مؤسسة ستا للمقاولات',
+        ]);
+        Supplier::create([
+            'name' => 'مؤسسة سدوك',
+        ]);
+        Supplier::create([
+            'name' => 'مؤسسة سدوك للمقاولات',
+        ]);
+        Supplier::create([
+            'name' => 'مؤسسة طريق الذكاء للتجارة',
+        ]);
+        Supplier::create([
+            'name' => 'مؤسسة منشأ البناء',
+        ]);
+        Supplier::create([
+            'name' => 'مؤسسة الحياة والحيوان للتجارة',
+        ]);
+        Supplier::create([
+            'name' => 'وكالة كوادر الاعلام ',
+        ]);
+        Supplier::create([
+            'name' => 'وكالة نحو الحوار ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة اللاستشاريون المحترفون ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة المعالم الدولية للتقنية ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة المعمر لأنظمة المعلومات ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة النهل للحاسب الآلي ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة بيرلز كونسلتنق السعودية ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة ثقة لخدمات الأعمال ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة دار البناء للمقاولات ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة شور ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة شور العالمية للتقنية ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة عالم الكمبيوتر الدولي ',
+        ]);
+        Supplier::create([
+            'name' => 'شركة محمد العلى السويلم للتجارة والمقاولات ',
+        ]);
+        Supplier::create([
+            'name' => 'الجديعي للأثاث المكتبي ',
+        ]);
+        Supplier::create([
+            'name' => 'الجريسي لخدمات الكمبيوتر والاتصالات ',
+        ]);
+
+        Supplier::create([
+            'name' => 'الشركة السعودية للتطوير العمراني',
+        ]);
+
+        Supplier::create([
+            'name' => 'الشركة الوطنية لأنظمة الحاسب الآلي',
+        ]);
+
+        Supplier::create([
+            'name' => 'شركة أيكوم العربية المحودة ',
+        ]);
+
+        Supplier::create([
+            'name' => 'شركة إي جي أند جي المتحدة ',
+        ]);
+
+        Supplier::create([
+            'name' => 'شركة الاتصال CCC ',
+        ]);
+
+        Supplier::create([
+            'name' => 'شركة الحلول الاولى ',
+        ]);
+
+        Supplier::create([
+            'name' => 'شركة الخدمات الاستشارية السعودية للإستشارات الهندسية (سعود كونسلت) ',
+        ]);
+
+        Supplier::create([
+            'name' => 'شركة القلعة للمستودعات (انفوفورت ',
+        ]);
+
+
+    }
+}
