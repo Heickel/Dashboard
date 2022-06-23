9a21058aad459635730f884fd043636c13e5ca23
nihelgh <73757674+nihelgh@users.noreply.github.com>
2022-06-23 13:32:50 +0100

    comments related to configuration in Project Controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,19 +8,42 @@ use App\Models\Project;
 
 class ProjectController extends Controller
 {
+    /**
+     * Get project form
+     * @method GET
+     * @return view
+     * @author Nihel Gharbi <gharbinihel1@gmail.com>
+     */
     public function index()
-    {
-        return view('add-project-details-form.index');
+    {   
+        try{
+        return view('projects.index');
+        }catch(Exception $ex){
+        Log::critical("create project error".$e->getMessage());
     }
-    
+    }
+    /**
+     * Get project form
+     * @method GET
+     * @return view
+     * @author Nihel Gharbi <gharbinihel1@gmail.com>
+     */
     public function create()
     {
         try{
-        return view('add-project-details-form.index');
+            return view('projects.index');
         }catch(Exception $ex){
-        Log::critical("create project error".$e->getMessage());
+            Log::critical("create project error".$e->getMessage());
         }
     }
+
+    /**
+     * Save project
+     * @method POST
+     * @param Request $request
+     * @return Response
+     * @author Nihel Gharbi <gharbinihel1@gmail.com>
+     */
     public function store(Request $request)
     {
         try{
@@ -30,23 +53,34 @@ class ProjectController extends Controller
             ]);           
 
            if(Project::create($request->all())){
-               return redirect()->route('add-project-details-form.index')->with('status', 'Projects Details Form Data Has Been inserted');
+               return redirect()->route('projects.index')->with('status', 'Projects Details Form Data Has Been inserted');
            }     
         }catch(Exception $e){
             Log::critical("message".$e->getMessage());
         }
     }
-
+    /**
+     * edit project
+     * @method GET
+     * @return view
+     * @author Nihel Gharbi <gharbinihel1@gmail.com>
+     */
     public function edit(Project $project)
     {
         try{        
-        return view('add-project-details-form.edit',compact('project'));
+        return view('projects.edit',compact('project'));
         }catch(Exception $e){
         Log::critical("edit project error".$e->getMessage());
         }
     }
 
-
+    /**
+     * Save project
+     * @method PUT
+     * @param Request $request , Project $project
+     * @return Response
+     * @author Nihel Gharbi <gharbinihel1@gmail.com>
+     */
     public function update(Request $request, Project $project)
     {
         try{
@@ -56,7 +90,7 @@ class ProjectController extends Controller
             ]);
     
         if($project->update($request->all())){
-            return redirect()->route('add-project-details-form.index')->with('status', 'Projects Details Form Data Has Been updated');
+            return redirect()->route('projects.index')->with('status', 'Projects Details Form Data Has Been updated');
         }     
         }catch(Exception $e){
                         Log::critical("message".$e->getMessage());
