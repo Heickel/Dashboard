cc4e29072d79e114c955687ef39ef7693713e22f
nihelgh <73757674+nihelgh@users.noreply.github.com>
2022-06-23 13:56:23 +0100

    add activity sector project seeder

---
+++ database/seeders/ActivitySectorProjectSeeder.php
@@ -0,0 +1,307 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ActivitySectorProject;
+
+
+class ActivitySectorProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ProgramCategory::create(
+        [
+            'title' => 'Authorized Economic Operator',
+            'programs_category_id' => '1',            
+            'rank' => '1',   
+        ],
+        [
+            'title' => 'Facilitate import and export procedures',
+            'programs_category_id' => '1',            
+            'rank' => '2',    
+        ],
+
+
+       [
+            'title' => 'Facilitate import and export procedures',
+            'programs_category_id' => '1',            
+            'rank' => '2',    
+       ],
+
+       [
+            'title' => 'Single window platform launch support',
+            'programs_category_id' => '1',            
+            'rank' => '3',
+        ],
+        [
+            'title' => 'Develop communication tools and communication channels with Saudi Customs clients',
+            'programs_category_id' => '2',            
+            'rank' => '4',
+        ],
+
+        [
+            'title' => 'Developing a corporate communication strategy and enhancing the identity of Saudi Customs', //5   
+            'programs_category_id' => '2',            
+            'rank' => '5',       
+        ],
+
+        [
+            'title' => 'Field Operations Center Development',
+            'programs_category_id' => '5',            
+            'rank' => '2',    
+        ], 
+
+        [
+            'title' => 'Develop security measures',
+            'programs_category_id' => '5',            
+            'rank' => '1',    
+        ],
+
+        [
+            'title' => 'Strengthen international cooperation and presence',    
+            'programs_category_id' => '3',            
+            'rank' => '8',
+        ],
+
+        [
+            'title' => 'Improving and developing the framework for customs brokers',    
+            'programs_category_id' => '1',            
+            'rank' => '9',
+        ],
+
+        [
+            'title' => 'Evaluate current sources of income and identify and apply additional sources of revenue',    
+            'programs_category_id' => '5',            
+            'rank' => '3',
+        ],
+
+        [
+            'title' => 'Support for the collection of value-added tax and excise tax',    
+            'programs_category_id' => '6',            
+            'rank' => '2',
+        ],
+
+        [
+            'title' => 'Identifying and implementing cost-reduction enablers',    
+            'programs_category_id' => '7',            
+            'rank' => '6',
+        ],
+
+        [
+            'title' => 'Partnerships between customs and the private sector',    
+            'programs_category_id' => '7',            
+            'rank' => '5',
+        ],
+
+        [
+            'title' => 'Providing new payment methods to collect revenue',    
+            'programs_category_id' => '8',            
+            'rank' => '4',
+        ],
+
+        [
+            'title' => 'Develop tariff and value procedures',    
+            'programs_category_id' => '8',            
+            'rank' => '7',
+        ],
+
+        [
+            'title' => 'Establishing and activating the post-customs audit',    
+            'programs_category_id' => '9',            
+            'rank' => '1',
+        ],
+
+        [
+            'title' => 'Internal process development',    
+            'programs_category_id' => '10',            
+            'rank' => '4',
+        ],
+
+        [
+            'title' => 'Develop legal procedures and framework for customs committees',    
+            'programs_category_id' => '11',            
+            'rank' => '2',
+        ],
+
+        [
+            'title' => 'Activate the strategic transformation management',    
+            'programs_category_id' => '12',            
+            'rank' => '1',
+        ],
+
+        [
+            'title' => 'Establishment of Risk Management, Compliance and Business Continuity',    
+            'programs_category_id' => '13',            
+            'rank' => '1',
+        ],
+
+        [
+            'title' => 'Developing a targeting mechanism risk engine)',    
+            'programs_category_id' => '13',            
+            'rank' => '2',
+        ],
+
+        [
+            'title' => 'Develop technical and digital strategy',    
+            'programs_category_id' => '14',            
+            'rank' => '9',
+        ],
+
+        [
+            'title' => 'Information technology infrastructure development',    
+            'programs_category_id' => '14',            
+            'rank' => '10',
+        ],
+
+        [
+            'title' => 'Development of the General Customs System Nebras 2.0)',    
+            'programs_category_id' => '14',            
+            'rank' => '12',
+        ],
+
+        [
+            'title' => 'Strengthening and developing the information security system',    
+            'programs_category_id' => '14',            
+            'rank' => '13',
+        ],
+
+        [
+            'title' => 'Data management and governance',    
+            'programs_category_id' => '14',            
+            'rank' => '5',
+        ],
+
+        [
+            'title' => 'Apply techniques to increase employee productivity',    
+            'programs_category_id' => '14',            
+            'rank' => '7',
+        ],
+
+        [
+            'title' => 'Implementation of solutions to support business operation',    
+            'programs_category_id' => '14',            
+            'rank' => '8',
+        ],
+
+        [
+            'title' => 'Building a disaster recovery center alternative data center)',    
+            'programs_category_id' => '14',            
+            'rank' => '6',
+        ],
+                
+        [
+            'title' => 'Development of the security technology system',    
+            'programs_category_id' => '14',            
+            'rank' => '11',
+        ],
+
+        [
+            'title' => 'Project management development, operation and maintenance',    
+            'programs_category_id' => '15',            
+            'rank' => '2',
+        ],
+                
+        [
+            'title' => 'Port development',    
+            'programs_category_id' => '15',            
+            'rank' => '3',
+        ],
+                
+        [
+            'title' => 'Enhanced safety level',    
+            'programs_category_id' => '15',            
+            'rank' => '4',
+        ],
+                
+        [
+            'title' => 'Achieving rapid gains for the needs of the outlets',    
+            'programs_category_id' => '15',            
+            'rank' => '1',
+        ],
+
+        [
+            'title' => 'Transition to the application of human resource management',    
+            'programs_category_id' => '16',            
+            'rank' => '1',
+        ],
+
+        [
+            'title' => 'Improving and creating the work environment',    
+            'programs_category_id' => '17',            
+            'rank' => '2',
+        ],
+
+        [
+            'title' => 'Enhance the level of employee engagement',    
+            'programs_category_id' => '17',            
+            'rank' => '3',
+        ],
+
+        [
+            'title' => 'Innovation platform development',    
+            'programs_category_id' => '17',            
+            'rank' => '5',
+        ],
+
+        [
+            'title' => 'Training development and competency management',    
+            'programs_category_id' => '18',            
+            'rank' => '4',
+        ],
+
+        [
+            'title' => 'Applications',    
+            'programs_category_id' => '19',            
+            'rank' => '1',
+        ],
+
+        [
+            'title' => 'Infrastructure',    
+            'programs_category_id' => '19',            
+            'rank' => '2',
+        ],
+
+        [
+            'title' => 'Other projects',    
+            'programs_category_id' => '19',            
+            'rank' => '3',
+        ],
+
+        [
+            'title' => 'live means',    
+            'programs_category_id' => '20',            
+            'rank' => '4',
+        ],
+
+        [
+            'title' => 'Other projects',    
+            'programs_category_id' => '20',            
+            'rank' => '5',
+        ],
+
+        [
+            'title' => 'Engineering Management',    
+            'programs_category_id' => '21',            
+            'rank' => '6',
+        ],
+
+        [
+            'title' => 'Operation and maintenance department',    
+            'programs_category_id' => '21',            
+            'rank' => '7',
+        ],
+
+        [
+            'title' => 'Corporate Communications',    
+            'programs_category_id' => '22',            
+            'rank' => '8',
+        ]);
+
+    }
+}
